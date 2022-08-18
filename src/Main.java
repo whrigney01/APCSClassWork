8becdd02edd0bd2bf4549ac9f9b6f0ac2cf46584
Will Rigney <73671586+whrigney01@users.noreply.github.com>
2022-08-18 14:42:54 -0600

    Update Main.java
    added a  new print message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a cool message");
+        System.out.println("This is a very cool message :) ");
     }
 }
