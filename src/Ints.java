555b6a405b1afe0a36494c9fdd881f35bb710b28
whrigney01 <whrigney01@bvsd.org>
2022-08-22 15:06:28 -0600

    added ints class and broke java multiple times java is bad at math

---
+++ src/Ints.java
@@ -0,0 +1,9 @@
+public class Ints {
+    public static void main(String[] args) {
+        String x = "\"Hi\"";
+        System.out.println(x);
+
+        x = x + "1";
+        System.out.println(x);
+    }
+}
