35a65ecd2a12f33b049d5f2b95f7551111f05505
Will Rigney <whrigney01@bvsd.org>
2022-08-24 19:31:28 -0600

    finished project 2

--- src/apcs/project1/Project2.java
+++ src/apcs/project1/Project2.java
@@ -1,13 +1,13 @@
 package apcs.project1;
 public class Project2 {
     public static void main(String[] args) {
-        System.out.println("        *        ");
-        System.out.println("      *  *      ");
-        System.out.println("     *    *     ");
-        System.out.println("    *      *    ");
-        System.out.println("   *        *   ");
-        System.out.println("  *          *  ");
-        System.out.println(" *            * ");
-        System.out.println("****************");
+        System.out.println("_________________________  ");
+        System.out.println(" \\      _________      /  ");
+        System.out.println("   \\    \\ YIELD /    /   ");
+        System.out.println("     \\    \\   /    /     ");
+        System.out.println("       \\   \\ /   /       ");
+        System.out.println("         \\     /          ");
+        System.out.println("           \\ /            ");
+
     }
 }
