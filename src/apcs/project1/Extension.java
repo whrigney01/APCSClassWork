bea548edef77f2d3bfcc77da7c8adf5785b7ee64
whrigney01 <whrigney01@bvsd.org>
2022-08-26 15:10:19 -0600

    Did project 3 and an Extension project This is my final work

---
+++ src/apcs/project1/Extension.java
@@ -0,0 +1,19 @@
+package apcs.project1;
+import java.util.Scanner;
+
+
+public class Extension {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Quadratic Formula");
+        System.out.print("a = ");
+        double a = reader.nextInt ();
+        System.out.print("b = ");
+        double b = reader.nextInt ();
+        System.out.print("c = ");
+        double c = reader.nextInt ();
+        double posQuadFormula = ((-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a));
+        double negQuadFormula = ((-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a));
+        System.out.println(posQuadFormula + "," + negQuadFormula);
+    }
+}
--- src/apcs/project1/Project3.java
+++ src/apcs/project1/Project3.java
@@ -1,7 +1,14 @@
 package apcs.project1;
+import java.util.Scanner;
+
 
 public class Project3 {
     public static void main(String[] args) {
-
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Kilometers to Nautical Miles Calculator");
+        System.out.print("Kilometers: ");
+        double km = reader.nextInt ();
+        double nauticalMiles = (km / 10000) * (60*90);
+        System.out.println("Nautical Miles: " + nauticalMiles);
     }
 }
