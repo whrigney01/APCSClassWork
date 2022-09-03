74ebb5622ccba81eb1c67f4f022b173b629c5c61
Will Rigney <whrigney01@bvsd.org>
2022-09-02 18:56:57 -0600

    finished in class work for 9/1

---
+++ src/Dice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Dice {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner (System.in);
+        int sides;
+        System.out.println("Enter a number of sides for the dice: ");
+        sides = reader.nextInt();
+        int die1 = (int) (Math.round(Math.random() * (sides - 1) + 1));
+        int die2 = (int) (Math.round(Math.random() * (sides - 1) + 1));
+        System.out.println("Die 1: " + die1);
+        System.out.println("Die 2: " + die2);
+        System.out.println("Total: " + (die1 + die2));
+
+    }
+}
--- src/Rand.java
+++ src/Rand.java
@@ -1,6 +1,16 @@
 public class Rand {
     public static void main(String[] args) {
-        double r = Math.random();
-        System.out.println(r);
+        double a = Math.random();
+        System.out.println("A random value between [0.0, 1.0) is " + a);
+        double b = Math.random() * 100.0;
+        System.out.println("A random value between [0.0, 100.0) is " + b);
+        double c = Math.random() + 5.0;
+        System.out.println("A random value between [5.0, 6.0) is " + c);
+        double d = Math.random() * 20.0 - 10.0;
+        System.out.println("A random value between [-10.0, 10.0) is " + d);
+        int f = (int) Math.round(Math.random() * 100.0);
+        System.out.println("A random value between [0, 100) is " + f);
+        int g = (int)(Math.random() * 51.0 + 1.0 );
+        System.out.println("A random value between [1, 52] is " + g);
     }
 }
