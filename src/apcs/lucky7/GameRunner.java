f86e11058154e01af21dc956fc961c71f7042919
Will Rigney <whrigney01@bvsd.org>
2022-09-14 06:37:54 -0600

    worked on the project

--- src/apcs/lucky7/GameRunner.java
+++ src/apcs/lucky7/GameRunner.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GameRunner {
     public static void main(String[] args) {
+        //Extension: I made the program have 2 games that you can play along with customizable options within the games like the winning value
         Scanner reader = new Scanner (System.in);
         double previousBalance = 0;
         double balance = 0.00;
@@ -13,24 +14,19 @@ public class GameRunner {
         int winValue = 0;
         int dieSides = 0;
         String result;
+        System.out.println("Do you want to play Lucky 7 or ");
         System.out.print("Starting Account Balance: $");
         balance = reader.nextInt();
         System.out.print("Winning Value: ");
         winValue = reader.nextInt();
-        System.out.print("Number of Sides on Die: ");
-        dieSides = reader.nextInt();
         System.out.println("Your starting account balance is $" + balance);
         System.out.println("The winning value is " + winValue);
-        System.out.println("The number of sides on the die is " + dieSides);
-        System.out.println("Are you ready to gamble away your life savings? Yes/No");
-        String yesNo = reader.nextStr();
-        if (yesNo.equals("Yes")) {
-            while (balance > 0) {
+        while (balance > 0) {
                 previousBalance = balance;
                 balance--;
                 int die1 = (int) (Math.random() * 6 + 1);
                 int die2 = (int) (Math.random() * 6 + 1);
-                if (die1 + die2 == 7) {
+                if (die1 + die2 == winValue) {
                     balance += 5;
                     result = "WIN!";
                 } else {
@@ -43,7 +39,6 @@ public class GameRunner {
                 }
                 System.out.println("Roll #" + timesPlayed + ": " + die1 + ", " + die2 + "   Total: " + (die1 + die2) + " = " + result + "  Starting Balance: $" + previousBalance + "0.  New Balance: $" + balance + "0.");
             }
-        }
         System.out.println("You should have stopped after " +maxRoll + " rolls when you balance was $" + accountMax + "0.");
     }
 }
