90e6d6234a64b0e1387dd22686e74a39023655e5
Will Rigney <whrigney01@bvsd.org>
2022-09-20 18:34:01 -0600

    worked on pi project

--- src/apcs/pi/PiRunner.java
+++ src/apcs/pi/PiRunner.java
@@ -6,21 +6,22 @@ public class PiRunner {
     public static void main(String[] args) {
         //part 1
         Scanner reader = new Scanner (System.in);
-//        System.out.println("Pi can be calculated by using an equation that alternates adding and subtracting descending fractions.");
-//        System.out.println("For example, Pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 ...");
-//        System.out.print("How many term do you want to add together? ");
-//        int numTerms = reader.nextInt();
+        System.out.println("Pi can be calculated by using an equation that alternates adding and subtracting descending fractions.");
+        System.out.println("For example, Pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 ...");
+        System.out.print("How many term do you want to add together? ");
+        int numTerms = reader.nextInt();
         double piApprox = 0.0;
         double fraction = 0.0;
         int posNeg = 1;
-//        for (double i = 1.0; i <= numTerms * 2; i += 2){
-//            fraction = 4/(i * posNeg);
-//            posNeg *= -1;
-//            piApprox += fraction;
-//
-//        }
-//        System.out.println("Pi approximated to " + numTerms + " terms is " + piApprox);
+        for (double i = 1.0; i <= numTerms * 2; i += 2){
+            fraction = 4/(i * posNeg);
+            posNeg *= -1;
+            piApprox += fraction;
+
+        }
+        System.out.println("Pi approximated to " + numTerms + " terms is " + piApprox);
        //part 2
+        System.out.println();
         System.out.println("Pi can also be calculated with the same equation but with a threshold of the smallest number, instead of a fixed amount of terms.");
         System.out.print("What should the smallest decimal possible be? ");
         double threshold = reader.nextDouble();
@@ -28,27 +29,32 @@ public class PiRunner {
         piApprox = 0.0;
         fraction = 1.0;
         posNeg = 1;
-        int numTerms = 0;
-        while (fraction > threshold){
-                fraction = 4 / (denom * posNeg);
+        numTerms = 0;
+        while (Math.abs(fraction) > threshold){
+                fraction = 4.0 / (denom * posNeg);
                 posNeg *= -1;
                 denom += 2;
-                piApprox += fraction ;
+                piApprox += fraction;
                 numTerms++;
             }
         System.out.println("Pi approximated until a term is less than " + threshold + " is " + piApprox);
         System.out.println("It required "+ numTerms + " terms to make this approximation");
-        //part 3 
+        //part 3
+        System.out.println();
         System.out.println("A 3rd way to approximate pi is to generate random coordinate pairs with in a box and see how many of those points fall with in a circle with a area of pi.");
-        System.out.println("How many coordinate pairs should be generated? ");
+        System.out.print("How many coordinate pairs should be generated? ");
         int numCoords = reader.nextInt();
-        int pointsInCircle = 0;
-        for (int i = 0; i > numCoords; i++){
+        double pointsInCircle = 0;
+        for (int i = 0; i < numCoords; i++){
             double xValue = (Math.random() * 2) - 1;
             double yValue = (Math.random() * 2) - 1;
-            if (Math.pow(xValue, 2) + Math.pow(yValue, 2) == Math.pow(1, 2)){
+            if ((Math.pow(xValue, 2) + Math.pow(yValue, 2)) < 1){
                 pointsInCircle ++;
             }
-        } 
-    }        
+        }
+        System.out.println(numCoords + " points were generated");
+        System.out.println((int)pointsInCircle + " were inside the circle");
+        System.out.println("Pi/4 is approximately " + (int) pointsInCircle + "/" + numCoords + " = " + (pointsInCircle/numCoords));
+        System.out.println(("Pi is approximately " + (pointsInCircle/numCoords)* 4.0));
+    }
 }
