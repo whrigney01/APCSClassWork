f5d0a98fe47d65a5ef585c448b91257bb6598cd4
[whrigney01]git <[Your>
2023-01-30 17:52:46 -0700

    help me

--- src/apcs/life/Cell.java
+++ src/apcs/life/Cell.java
@@ -1,6 +1,7 @@
 package apcs.life;
 
 import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 import java.lang.reflect.Array;
@@ -8,52 +9,55 @@ import java.util.ArrayList;
 
 public class Cell extends Actor {
     private int phase;
-    private int lifeOrDeath;
-    //1 = dead and 0 = life
+    private boolean isDied;
+
     private ArrayList<Location> locs;
 
     public Cell() {
         phase = 1;
-
+        locs = new ArrayList<Location>();
 
     }
 
     public void act() {
         if (this.phase == 1) {
-            this.phase1();
             this.phase = 2;
-        } else {
-            this.phase2();
+            this.phase1();
+        } else if(this.phase == 2){
             this.phase = 1;
+            this.phase2();
         }
     }
 
     public void phase1() {
-        ArrayList<Actor> neighbors = this.getGrid().getNeighbors(this.getLocation());
-        if (neighbors.size() == 2 || neighbors.size() == 3) {
-            lifeOrDeath = 0;
+        Grid<Actor> gr = this.getGrid();
+
+        if (gr.getNeighbors(this.getLocation()).size() == 2 || gr.getNeighbors(this.getLocation()).size() == 3) {
+            isDied = false;
         } else {
-            lifeOrDeath = 1;
+            isDied = true;
         }
-        ArrayList<Location> emptyLocs = this.getGrid().getEmptyAdjacentLocations(this.getLocation());
+        ArrayList<Location> emptyLocs = gr.getEmptyAdjacentLocations(this.getLocation());
         for (int i = 0; i < emptyLocs.size(); i++) {
-            if(this.getGrid().getNeighbors(emptyLocs.get(i)).size() == 3){
+            if (gr.getNeighbors(emptyLocs.get(i)).size() == 3) {
+
                 locs.add(emptyLocs.get(i));
             }
         }
     }
 
     public void phase2() {
-        if(this.lifeOrDeath == 1) {
+        Grid<Actor> gr = this.getGrid();
+        if (this.isDied) {
             this.removeSelfFromGrid();
-        }
+        } else {
 
-        for(int i = 0; i < locs.size(); i++){
-            if(!(this.getGrid().get(locs.get(i)) instanceof Actor)){
-                Cell c = new Cell();
-                c.putSelfInGrid(this.getGrid(), locs.get(i));
+            for (int i = 0; i < locs.size(); i++) {
+                if (!(gr.get(locs.get(i)) instanceof Actor)) {
+                    Cell c = new Cell();
+                    c.putSelfInGrid(gr, locs.get(i));
+                }
             }
-
 
         }
     }
