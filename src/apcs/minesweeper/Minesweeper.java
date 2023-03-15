31fb1d1d5afd351351273ea3929e86a8663856bc
[whrigney01]git <[Your>
2023-03-14 20:47:45 -0600

    forloophell

--- src/apcs/minesweeper/Minesweeper.java
+++ src/apcs/minesweeper/Minesweeper.java
@@ -151,13 +151,61 @@ public class Minesweeper {
                         board[r1][c1].reveal();
                     } else if (board[r1][c1].getNumOfBomb() == 0 && oldRow != r1 && oldCol != c1  && !board[r1][c1].isBomb()) {
                         board[r1][c1].reveal();
-                        for (int r2 = r1 - 1; r2 <= row + 1; r2++) {
-                            for (int c2 = c1 - 1; c2 <= col + 1; c2++) {
+                        for (int r2 = r1 - 1; r2 <= r1 + 1; r2++) {
+                            for (int c2 = c1 - 1; c2 <= c1 + 1; c2++) {
                                 if (!(r2 < 0 || r2 > size - 1 || c2 < 0 || c2 > size - 1)) {
                                     if (board[r2][c2].getNumOfBomb() != 0  && !board[r2][c2].isBomb()) {
                                         board[r2][c2].reveal();
                                     } else if (board[r2][c2].getNumOfBomb() == 0 && oldRow != r2 && oldCol != c2 && !board[r2][c2].isBomb()) {
                                         board[r2][c2].reveal();
+                                        for (int r3 = r2 - 1; r3 <= r2 + 1; r3++) {
+                                            for (int c3 = c2 - 1; c3 <= c2 + 1; c3++) {
+                                                if (!(r3 < 0 || r3 > size - 1 || c3 < 0 || c3 > size - 1)) {
+                                                    if (board[r3][c3].getNumOfBomb() != 0  && !board[r3][c3].isBomb()) {
+                                                        board[r3][c3].reveal();
+                                                    } else if (board[r3][c3].getNumOfBomb() == 0 && oldRow != r3 && oldCol != c3 && !board[r3][c3].isBomb()) {
+                                                        board[r3][c3].reveal();
+                                                        for (int r4 = r3 - 1; r4 <= r3 + 1; r4++) {
+                                                            for (int c4 = c3 - 1; c4 <= c3 + 1; c4++) {
+                                                                if (!(r4 < 0 || r4 > size - 1 || c4 < 0 || c4 > size - 1)) {
+                                                                    if (board[r4][c4].getNumOfBomb() != 0  && !board[r4][c4].isBomb()) {
+                                                                        board[r4][c4].reveal();
+                                                                    } else if (board[r4][c4].getNumOfBomb() == 0 && oldRow != r4 && oldCol != c4 && !board[r4][c4].isBomb()) {
+                                                                        board[r4][c4].reveal();
+                                                                        for (int r5 = r4 - 1; r5 <= r4 + 1; r5++) {
+                                                                            for (int c5 = c4 - 1; c5 <= c4 + 1; c5++) {
+                                                                                if (!(r5 < 0 || r5 > size - 1 || c5 < 0 || c5 > size - 1)) {
+                                                                                    if (board[r5][c5].getNumOfBomb() != 0  && !board[r5][c5].isBomb()) {
+                                                                                        board[r5][c5].reveal();
+                                                                                    } else if (board[r5][c5].getNumOfBomb() == 0 && oldRow != r5 && oldCol != c5 && !board[r5][c5].isBomb()) {
+                                                                                        board[r5][c5].reveal();
+                                                                                        for (int r6 = r5 - 1; r6 <= r5 + 1; r6++) {
+                                                                                            for (int c6 = c5 - 1; c6 <= c5 + 1; c6++) {
+                                                                                                if (!(r6 < 0 || r6 > size - 1 || c6 < 0 || c6 > size - 1)) {
+                                                                                                    if (board[r6][c6].getNumOfBomb() != 0  && !board[r6][c6].isBomb()) {
+                                                                                                        board[r6][c6].reveal();
+                                                                                                    } else if (board[r6][c6].getNumOfBomb() == 0 && oldRow != r6 && oldCol != c6 && !board[r6][c6].isBomb()) {
+                                                                                                        board[r6][c6].reveal();
+                                                                                                    }
+
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+
+                                                }
+                                            }
+                                        }
                                     }
 
                                 }
